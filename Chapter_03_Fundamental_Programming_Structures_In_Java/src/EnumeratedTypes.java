c52f117aa3cf8d6bedf7c43bd6fa831f788674b9
Tri Phan <phanphutri@gmail.com>
2021-09-13 19:37:06 +0700

    Updated

---
+++ Chapter_03_Fundamental_Programming_Structures_In_Java/src/EnumeratedTypes.java
@@ -0,0 +1,13 @@
+
+public class EnumeratedTypes 
+{
+	public static void main(String[] args) 
+	{
+//		Defines a enumerated type named "Size"
+		enum Size {SMALL, MEDIUM, LARGE, EXTRA_LARGE}
+		
+//		Declares a variable of this type
+		Size s = Size.MEDIUM;
+	}
+	
+}
